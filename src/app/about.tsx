709fa2fa6863917e2cf218d3f1c8aa34efcc6822
nahid-diu-cse <nahid.diu.cse@gmail.com>
2024-11-24 19:01:00 +0600

    2 page done

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -1,5 +1,14 @@
-import { Text } from "react-native";
+import { Button, Text } from "react-native";
+import { View,Image } from "react-native";
 export default function HomeScreen() {
-    return  <Text  style={{color:"red", }}>About page</Text>
-   
+    return  (
+        <View style={{flex:1,display:"flex",justifyContent:"center",alignItems:"center", backgroundColor:"#fff"}}>
+            <Image height={50} width={50} resizeMode="contain" className="m-4" source={require("./../../assets/favicon.png")} />
+            <Text className="text-2xl">About This App</Text>
+            <Text className="text-lg p-4 w-full leading-8 tracking-wide">Culpa quis mollit eu sunt consectetur in ea pariatur. Qui do deserunt magna adipisicing esse ullamco aliqua anim et anim. Excepteur commodo eu pariatur ad labore elit esse est. Laboris incididunt excepteur incididunt adipisicing magna enim mollit nisi cillum.</Text>
+            <Button  title="Go Back"  />
+            
+        </View>
+    
+    )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,29 +1,23 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { FlatList, Text, View , Image} from "react-native";
 import { meditation, } from "../data";
 
 const data = meditation[2]
 
+function meditationList(data) {
+    return(
+        <View className="p-4 m-4 rounded-xl border-2 border-slate-400 bg-teal-100  " >
+            <Image  height={50} width={50} resizeMode="contain" className="m-4 " source={require("./../../assets/favicon.png")} />
+            <Text className="bg-slate-100 text-xl    rounded-xl font-semibold p-2" >{data.title}</Text>
+            <Text className="p-2">{data.description}</Text>
+        </View>
+    )
+}
+
 export default function HomeScreen() {
     return(
-        <View style={{flex:1,display:"flex",justifyContent:"flex-start",alignItems:"flex-start",backgroundColor:"#fff", paddingTop:16 , 
-        }}>
-            
-    {
-         meditation.map((item) => {
-             return(
-
-          
-
-            <View key={item.id} className="flex flex-col border-gray-100 border-2  w-full bg-teal-50 rounded-xl   p-4 m-1 gap-4">
-                  <Text className="bg-slate-100 text-xl    rounded-xl" >{item.title}</Text>
-                  <Text >{item.description}</Text>
-            </View>
-            
-                )
-         })
-    }
-
+        <View >
+            <FlatList className="h-5/6 " data={meditation} renderItem={({item}) => meditationList(item)} />
 
             <Link className="text-2xl p-4 text-blue-500" href="/about">about page</Link>
         </View>
